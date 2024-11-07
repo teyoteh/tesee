f68b483fe3c57c43018a206d3d48e0869a9cbf8b
thingsteyot <153864999+thingsteyot@users.noreply.github.com>
2024-11-07 12:39:17 +0300

    dn

---
+++ src/games/newGame/GameController.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface GameControllerProps {
+  result: string;
+}
+
+export const GameController: React.FC<GameControllerProps> = ({ result }) => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <h2>{result}</h2>
+    </div>
+  );
+};
+ 
